24424494b8806252c1b97c6e152189f83a8ae0f3
alexajaime09 <alexaquezada23@gmail.com>
2024-06-23 01:01:16 -0600

    languages test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const button = document.getElementById('button');
 const idioms = document.getElementById('container-idioms')
+const leng = document.querySelectorAll('#leng a');
 
 
 button.addEventListener('click',function() {
@@ -10,4 +11,23 @@ button.addEventListener('click',function() {
     else{
         idioms.style.display = 'none';
     }
+})
+
+
+
+leng.forEach((link) =>{
+    link.addEventListener('click', (event)=>{
+        const lang = event.target.textContent;
+
+        if(lang === 'French'){
+            location.href="./french/french.html"
+        }
+        else if(lang ==='Spanish'){
+            location.href="./spanish/spanish.html"
+        }
+
+        else {
+            location.href="./index.html"
+        }
+    })
 })
